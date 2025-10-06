0715c8b25e19471312bd485b7aa7384795272fd1
alfonWaz13 <alfonWaz13@icloud.com>
2025-10-06 15:45:08 +0200

    feat: add all urls table

--- links/views.py
+++ links/views.py
@@ -4,6 +4,7 @@ from django.views import View
 
 from links.repositories.link_repository import LinkRepository
 from links.use_cases.create_short_url import CreateShortURL
+from links.use_cases.get_all_urls import GetAllURLs
 
 
 class IndexView(View):
@@ -14,15 +15,20 @@ class IndexView(View):
     create_short_url = CreateShortURL(
         link_repository=link_repository, url_shortener=url_shortener
     )
+    get_all_urls = GetAllURLs(link_repository=link_repository)
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        return render(
+            request, self.template_name, {"all_urls": self.get_all_urls.execute()}
+        )
 
     def post(self, request, *args, **kwargs):
+        response_context = {}
         long_url = request.POST.get("url_input")
+
         if long_url:
-
             short_url = self.create_short_url.execute(long_url=long_url)
-            return render(request, self.template_name, {"short_url": short_url})
+            response_context["short_url"] = short_url
 
-        return render(request, self.template_name)
+        response_context["all_urls"] = self.get_all_urls.execute()
+        return render(request, self.template_name, response_context)
