86136839f16ea4abc4b4eec747dd621f5e0c2fd7
alfonWaz13 <alfonWaz13@icloud.com>
2025-10-06 11:14:20 +0200

    feat: add new use case to application

--- links/views.py
+++ links/views.py
@@ -1,4 +1,20 @@
+import pyshorteners
 from django.shortcuts import render
 
+from links.repositories.link_repository import LinkRepository
+from links.use_cases.create_short_url import CreateShortURL
+
+
 def index(request):
+    if request.method == 'POST':
+        long_url = request.POST['url_input']
+
+        link_repository = LinkRepository()
+        url_shortener = pyshorteners.Shortener()
+        create_short_url_use_case = CreateShortURL(
+            link_repository=link_repository,
+            url_shortener=url_shortener
+        )
+        short_url = create_short_url_use_case.execute(long_url=long_url)
+        return render(request, 'links/index.html', {'short_url': short_url})
     return render(request, 'links/index.html')
